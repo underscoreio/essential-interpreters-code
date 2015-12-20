29b7f3eb851597777e0e77caa2995366b6ec8fac
Noel Welsh <noel@untyped.com>
2015-12-20 22:08:06 +0000

    Correct terminology, renaming Value => Literal

--- untyped/src/main/scala/untyped/Arithmetic.scala
+++ untyped/src/main/scala/untyped/Arithmetic.scala
@@ -8,7 +8,7 @@ object Arithmetic {
         case Minus(l, r)    => l.eval - r.eval
         case Multiply(l, r) => l.eval * r.eval
         case Divide(l, r)   => l.eval / r.eval
-        case Value(v)       => v
+        case Literal(v)       => v
       }
 
     def +(that: Expression): Expression =
@@ -21,14 +21,14 @@ object Arithmetic {
       Divide(this, that)
   }
   object Expression {
-    // Smart constructor that creates Value with type Expression
-    def value(in: Double): Expression =
-      Value(in)
+    // Smart constructor that creates a Literal with type Expression
+    def literal(in: Double): Expression =
+      Literal(in)
   }
   final case class Plus(left: Expression, right: Expression) extends Expression
   final case class Minus(left: Expression, right: Expression) extends Expression
   final case class Multiply(left: Expression, right: Expression) extends Expression
   final case class Divide(left: Expression, right: Expression) extends Expression
-  final case class Value(get: Double) extends Expression
+  final case class Literal(get: Double) extends Expression
 
 }
